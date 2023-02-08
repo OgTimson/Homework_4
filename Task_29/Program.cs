5efd576f460f25b1c4be3eb6d374a7fd4775ed30
Timofey <ogtimson@gmail.com>
2023-02-08 23:04:55 +0600

    The Task 29 is ready

---
+++ Task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] originalArray = { 1, 2, 5, 7, 19, 6, 1, 33 };
+int[] firstArray = new int[5];
+int[] secondArray = new int[3];
+
+for (int i = 0; i < originalArray.Length; i++)
+{
+    if (i < 5)
+    {
+        firstArray[i] = originalArray[i];
+    }
+    else
+    {
+        secondArray[i - 5] = originalArray[i];
+    }
+}
+Console.WriteLine("Первые 5 элементов массива из 8 элементов:");
+Console.WriteLine("[" + String.Join(", ", firstArray) + "]");
+Console.WriteLine("\nПоследние 3 элемента массива из 8 элементов:");
+Console.WriteLine("[" + String.Join(", ", secondArray) + "]");
