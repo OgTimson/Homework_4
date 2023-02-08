25309394f9cc6671b1c724dfd678ed2014744007
Timofey <ogtimson@gmail.com>
2023-02-08 21:49:17 +0600

    The Task 27 is ready

---
+++ Task_27/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или число меньше 0. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int GetSum(int number)
+{
+    int sum = 0;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        sum = sum + digit;
+        number = number / 10;
+    }
+
+    return sum;
+}
+
+int number = GetNumber("Введите положительное число");
+int sumDigit = GetSum(number);
+
+Console.WriteLine($"Сумма цифр в числе {number} = {sumDigit}");
